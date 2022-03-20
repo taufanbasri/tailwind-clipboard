9bc8cd515f342e72add60102e349846cfbbe2de0
taufanbasri <taufanprasetyobasri@gmail.com>
2022-03-20 21:05:40 +0700

    Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(171, 66%, 44%)",
+        "light-blue": "hsl(233, 100%, 69%)",
+        "dark-gray-blue": "hsl(210, 10%, 33%)",
+        "grey-blue": "hsl(201, 11%, 66%)",
+      },
+      backgroundImage: {
+        hero: "url('/assets/bg-header-desktop.png')",
+      }
+    },
+    fontFamily: {
+      BaiJamjuree: ['"Bai Jamjuree"', 'sans-serif']
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: "1440px",
+        xl: "1440px",
+        '2xl': "1440px"
+      }
+    }
   },
   plugins: [],
 }
